d1adf5fb240f747cd0dfda800da6831b130aa9f7
Arshak Asriyan <aasriyan01@gmail.com>
2022-09-07 18:29:14 -0400

    repo init

---
+++ components/NavBar.tsx
@@ -0,0 +1,17 @@
+import { WalletDisconnectButton } from "@solana/wallet-adapter-react-ui";
+import { useWallet } from "@solana/wallet-adapter-react";
+
+import styles from "../styles/NavBar.module.css";
+
+const NavBar = () => {
+	const { publicKey } = useWallet();
+
+	return (
+		<div className={styles.navbar}>
+			<h1>Solana Wallet Task</h1>
+			{publicKey && <WalletDisconnectButton />}
+		</div>
+	);
+};
+
+export default NavBar;
