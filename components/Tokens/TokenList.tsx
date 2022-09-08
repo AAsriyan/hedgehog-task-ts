1b1b44dd10f3ce0dd07a0bb4326ea8e4829ac7f5
Arshak Asriyan <aasriyan01@gmail.com>
2022-09-08 08:21:27 -0400

    added in loading states for token list component

--- components/Tokens/TokenList.tsx
+++ components/Tokens/TokenList.tsx
@@ -17,6 +17,7 @@ export type TokenItem = {
 
 const TokenList = ({ copyText }: TokenListProps) => {
 	const [tokenList, setTokenList] = useState<TokenItem[]>([]);
+	const [isLoading, setIsLoading] = useState(false);
 	const { connection } = useConnection();
 	const { publicKey } = useWallet();
 
@@ -24,6 +25,7 @@ const TokenList = ({ copyText }: TokenListProps) => {
 		if (!publicKey) {
 			return;
 		}
+		setIsLoading(true);
 		const getTokenList = async () => {
 			const tokenAccounts = await connection.getTokenAccountsByOwner(
 				publicKey,
@@ -42,7 +44,7 @@ const TokenList = ({ copyText }: TokenListProps) => {
 					account: accountData
 				};
 			});
-
+			setIsLoading(false);
 			setTokenList(parsedTokenAccounts);
 		};
 
@@ -51,15 +53,21 @@ const TokenList = ({ copyText }: TokenListProps) => {
 
 	return (
 		<Card>
-			{tokenList.map((token) => {
-				return (
-					<Token
-						key={token.pubkey.toBase58()}
-						tokenInfo={token}
-						copyText={copyText}
-					/>
-				);
-			})}
+			{isLoading && <p>Loading...</p>}
+			{!isLoading && tokenList.length < 1 && (
+				<p>You have no tokens at the moment.</p>
+			)}
+			{!isLoading &&
+				tokenList.length > 1 &&
+				tokenList.map((token) => {
+					return (
+						<Token
+							key={token.pubkey.toBase58()}
+							tokenInfo={token}
+							copyText={copyText}
+						/>
+					);
+				})}
 		</Card>
 	);
 };
